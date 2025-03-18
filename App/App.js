0c26fd892b40a7b8b67ac24e8f2112ddcb23b6a6
KrryStvn <0322103718@ut-tijuana.edu.mx>
2025-03-18 12:22:18 -0700

    hi

--- App/App.js
+++ App/App.js
@@ -1,20 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, TextInput, Button, View, Image } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const image = require('./Logo/VitalRisk.jpg');
+
+
+    return (
+        <View style={styles.container}>
+            <Image style={styles.image} source={image} />
+
+            <TextInput
+                style={styles.input}
+                placeholder="Enter ID"
+                
+            />
+            <TextInput
+                style={styles.input}
+                placeholder="Enter password"
+                
+            />
+           
+            <Button title="Sign Up" color="#23998E" />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 16,
+    },
+    input: {
+        width: '80%',
+        height: 40,
+        borderWidth: 1,
+        borderColor: 'gray',
+        marginBottom: 16,
+        paddingLeft: 8,
+    },
+    resultText: {
+        width: '80%',
+        marginTop: 16,
+        fontSize: 16,
+    },
+    image: {
+        width: 200,
+        height: 100,
+        marginBottom: 50,
+    },
 });
+
